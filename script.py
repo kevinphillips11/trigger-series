af65fad15e2de9cd49e7cfdd9785e3e81df05bf6
kevinphillips11 <150522296+kevinphillips11@users.noreply.github.com>
2023-11-28 11:27:22 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,49 @@
-print('hello')
+import requests
+import json
+import os
+
+# cancel the previous run
+
+url = f"https://api.github.com/repos/kevinphillips11/REPO/actions/runs/{os.environ['PREVIOUS_RUN_ID']}/cancel"
+
+payload = {}
+headers = {
+  'Accept': 'application/vnd.github+json',
+  'Authorization': 'Bearer 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',
+  'X-GitHub-Api-Version': '2022-11-28'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+
+# middle part - print hello
+
+print('hello!')
+
+# trigger the next run
+
+url = "https://api.github.com/repos/kevinphillips11/trigger-series/actions/workflows/run.yml/dispatches"
+
+payload = json.dumps({
+"ref": "main",
+"inputs": {
+    "manual_trigger": "true",
+    "previous_run_id": os.environ['CURRENT_RUN_ID']
+}
+})
+headers = {
+'Accept': 'application/vnd.github+json',
+'Authorization': 'Bearer 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',
+'X-GitHub-Api-Version': '2022-11-28',
+'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+
+# wait to be canceled by the next run
+
+while True:
+    pass
